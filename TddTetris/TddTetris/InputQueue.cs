1543ad92e248b94b524bd79d5d1ec7bbec081ee1
Lennaert Meijvogel <l.meijvogel@ogd.nl>
2011-11-11 12:58:50 +0100

    Improve comments on the InputQueue class.

--- TddTetris/TddTetris/InputQueue.cs
+++ TddTetris/TddTetris/InputQueue.cs
@@ -7,11 +7,15 @@ using Microsoft.Xna.Framework.Input;
 namespace TddTetris
 {
     /**
-     * Make sure that any keypresses are recorded only once
+     * Make sure that any keypresses are recorded only once. XNA 'repeats' keypresses at every iteration.
+     *
+     * At some point, I'd like to add repetition.
      */
     public class InputQueue
     {
         private List<Keys> lastKeys = new List<Keys>();
+
+        // Returns only the keys that were pressed keys since its last invocation.
         public List<Keys> keyPress(List<Keys> keys)
         {
             var newlyPressedKeys = keys.Except(lastKeys);
